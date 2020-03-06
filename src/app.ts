dcd627ea6ceaa7e6c8bfa00b3831e20311d13496
angel-torres <alas4224@gmail.com>
2020-03-05 21:21:51 -0600

    added Request and Response types to get request

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express = require('express');
+import { Request, Response } from 'express';
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send("<h1>Hello world!</h1>");
 });
 
