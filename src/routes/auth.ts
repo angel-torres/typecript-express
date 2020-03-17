0507740bb24f1e74118f09a33077be0f34ca9939
angel-torres <alas4224@gmail.com>
2020-03-17 17:12:17 -0500

    working on fixing a bug that does not let users log in

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,24 +6,50 @@ const { User } = require('../models/Schemas');
 const authRoute = express.Router();
 
 authRoute.post('/signup', async (req: Request, res: Response) => {
-    try {
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync("B4c0/\/", salt);
-        const newUser = {
-            username: req.body.username,
-            password: hash,
+    if(req.body.username && req.body.password) {
+        const {username, password} = req.body;
+        try {
+            const user = await User.find({username: username});
+            if (user.length > 0) {
+                res.status(400).json({message: "Username already exists"});
+            } else {
+                const hash = await bcrypt.hash(password, 10);
+                const newUser = {
+                    username: req.body.username,
+                    password: hash,
+                }
+                const userEntry = new User (newUser);
+                const newEntry = await userEntry.save();
+                res.status(200).json({user: "User created."});
+            }
+        } catch (err) {
+            res.send({error: err})
         }
-        const userEntry = new User (newUser);
-        const newEntry = await userEntry.save();
-        res.status(200).json(newEntry);
-    } catch (err) {
-        res.send({error: err})
+    } else {
+        res.status(400).json({message: "Must provide username and passord."})
     }
 })
 
-authRoute.post('/login', (req: Request, res: Response) => {
-    console.log("We are logging in!!");
-    res.send("<h1>Logging in!!</h1>");
+authRoute.post('/login', async (req: Request, res: Response) => {
+    if(req.body.username && req.body.password) {
+        const { username, password } = req.body;
+        console.log("username", username);
+        console.log("password", password);
+        try {
+            const user = await User.find({username: username});
+            if (user.length > 0) {
+                const isPasswordValid = await bcrypt.compare(password, user.password);
+                console.log("isPasswordValid - ", isPasswordValid)
+            } else {
+                res.status(400).json({message: "User not found."});
+            }
+        } catch (err) {
+            console.log("error - ", err);
+            res.send({error: err})
+        }
+    } else {
+        res.status(400).json({message: "Must provide username and password."});
+    }
 });
 
 export default authRoute;
