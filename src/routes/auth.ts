d85468732acf9847618e4e76e0acec7cacde6602
angel-torres <alas4224@gmail.com>
2020-03-18 00:10:02 -0500

    removed some more logic from auth endpoints and used middleware to clean things up

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,28 +6,24 @@ import { authValidation } from "../middleware/authValidation";
 
 const authRoute = express.Router();
 
-authRoute.post('/signup', async (req: Request, res: Response) => {
-    if(req.body.username && req.body.password) {
-        const {username, password} = req.body;
-        try {
-            const user = await User.find({username: username});
-            if (user.length > 0) {
-                res.status(400).json({message: "Username already exists"});
-            } else {
-                const hash = await bcrypt.hash(password, 10);
-                const newUser = {
-                    username: req.body.username,
-                    password: hash,
-                }
-                const userEntry = new User (newUser);
-                const newEntry = await userEntry.save();
-                res.status(200).json({user: "User created."});
+authRoute.post('/signup',authValidation, async (req: Request, res: Response) => {
+    const {username, password} = req.body;
+    try {
+        const user = await User.find({username: username});
+        if (user.length > 0) {
+            res.status(400).json({message: "Username already exists"});
+        } else {
+            const hash = await bcrypt.hash(password, 10);
+            const newUser = {
+                username: req.body.username,
+                password: hash,
             }
-        } catch (err) {
-            res.send({error: err})
+            const userEntry = new User (newUser);
+            const newEntry = await userEntry.save();
+            res.status(200).json({user: "User created."});
         }
-    } else {
-        res.status(400).json({message: "Must provide username and passord."})
+    } catch (err) {
+        res.send({error: err})
     }
 })
 
@@ -46,7 +42,6 @@ authRoute.post('/login', authValidation, async (req: Request, res: Response) => 
             res.status(400).json({message: "User not found."});
         }
     } catch (err) {
-        console.log("error - ", err);
         res.send({error: err})
     }
 });
