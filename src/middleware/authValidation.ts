c5e49220fbc1371aba66ab4c6069db1c2dc5bbbe
angel-torres <alas4224@gmail.com>
2020-03-20 22:53:08 -0500

    refactored a few things

--- src/middleware/authValidation.ts
+++ src/middleware/authValidation.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 export function authValidation(request: Request, response: Response, next: Function) {
-    if(request.body.username && request.body.password) {
+    const { username, password } = request.body;
+    if( username && password ) {
         next();
     } else {
-        response.status(400).json({message: "Must provide username and password."});
+        response
+        .status(400)
+        .json({message: "Must provide username and password."});
     }
 };
