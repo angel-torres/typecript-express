ffe500ba0b9432e201d1da81e69acb07331d9e21
angel-torres <alas4224@gmail.com>
2020-03-20 22:49:49 -0500

    updated name variables in auth route for cleaner code

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,43 +6,43 @@ import { authValidation } from "../middleware/authValidation";
 
 const authRoute = express.Router();
 
-authRoute.post('/signup',authValidation, async (req: Request, res: Response) => {
-    const {username, password} = req.body;
+authRoute.post('/signup',authValidation, async (request: Request, response: Response) => {
+    const {username, password} = request.body;
     try {
         const user = await User.find({username: username});
         if (user.length > 0) {
-            res.status(400).json({message: "Username already exists"});
+            response.status(400).json({message: "Username already exists"});
         } else {
             const hash = await bcrypt.hash(password, 10);
             const newUser = {
-                username: req.body.username,
+                username: request.body.username,
                 password: hash,
             }
             const userEntry = new User (newUser);
             const newEntry = await userEntry.save();
-            res.status(200).json({user: "User created."});
+            response.status(200).json({user: "User created."});
         }
-    } catch (err) {
-        res.send({error: err})
+    } catch (error) {
+        response.send({error})
     }
 })
 
-authRoute.post('/login', authValidation, async (req: Request, res: Response) => {
-    const { username, password } = req.body;
+authRoute.post('/login', authValidation, async (request: Request, response: Response) => {
+    const { username, password } = request.body;
     try {
         const [ user ] = await User.find({username: username});
         if (user) {
             const isPasswordValid = await bcrypt.compare(password, user.password);
             if(isPasswordValid){
-                res.status(200).json({message: "You are logged in!"})
+                response.status(200).json({message: "You are logged in!"})
             } else {
-                res.status(400).json({message: "Invalid password."})
+                response.status(400).json({message: "Invalid password."})
             }
         } else {
-            res.status(400).json({message: "User not found."});
+            response.status(400).json({message: "User not found."});
         }
-    } catch (err) {
-        res.send({error: err})
+    } catch (error) {
+        response.send({error})
     }
 });
 
