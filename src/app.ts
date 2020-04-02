efdaef5b74b792634def62244eceb308ff7a4113
angel-torres <alas4224@gmail.com>
2020-04-02 14:12:44 -0500

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -18,11 +18,11 @@ mongoose.connect(process.env.DB_CONNECTION, {
 
 // INTIALIZING EXPRESS APP
 const app = express();
+app.use(cors());
 const port: number =  parseInt(process.env.PORT) || 3000;
 app.set("port", process.env.PORT || 3000);
 
 // MIDDLEWARE
-app.use(cors());
 app.use(express.json());
 app.use(cookeParser());
 app.use(helmet());
